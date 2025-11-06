25cfbc92434193e6ac37856db8ce42640ed380cd
Your Name <alisapodgaynaya023@gmail.com>
2025-11-06 08:46:52 -0700

    add smooth-scroll, close on esc, body no scroll

--- src/js/header.js
+++ src/js/header.js
@@ -4,16 +4,24 @@ import { header, burgerButton, closeButton, closeButtonTab } from './refs';
 // EVENT LISTENERS
 
 header.addEventListener('click', toggleNavMenu);
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && header.classList.contains('menu-open')) {
+    header.classList.remove('menu-open');
+    document.body.classList.remove('no-scroll');
+  }
+});
 
 // RENDER
 export function toggleNavMenu(event) {
   if (burgerButton.contains(event.target)) {
     header.classList.add('menu-open');
+    document.body.classList.add('no-scroll');
   } else if (
     closeButton.contains(event.target) ||
     closeButtonTab.contains(event.target)
   ) {
     header.classList.remove('menu-open');
+    document.body.classList.remove('no-scroll');
   } else {
     return;
   }
